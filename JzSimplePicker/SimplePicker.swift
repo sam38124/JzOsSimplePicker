60084a1e0244eced3c2f41449179d71c2a734349
sam38124 <sam38124@gmail.com>
2020-04-11 05:48:51 -0700

    0411

--- JzSimplePicker/SimplePicker.swift
+++ JzSimplePicker/SimplePicker.swift
@@ -12,7 +12,9 @@ public class SimplePicker:NSObject,UIPickerViewDelegate,UIPickerViewDataSource{
     var item:[String]! = nil
     var callback: ((_ position:String) -> Void)!=nil
     var picker=UIPickerView()
-    public init(_ view:UIView,_ item:[String],_ selectLinstener:@escaping (_ position:String) -> Void) {
+    var button:UIButton! = nil
+    public init(_ view:UIView,_ item:[String],_ selectLinstener:@escaping (_ position:String) -> Void,_ donecolor:UIColor=UIColor.black) {
+        super.init()
         callback=selectLinstener
         self.item=item
         picker.backgroundColor = .white
@@ -20,14 +22,22 @@ public class SimplePicker:NSObject,UIPickerViewDelegate,UIPickerViewDataSource{
         //        let color2 = UIColor.white
         picker.setValue(color1, forKey: "textColor")
         picker.frame=CGRect(x: 0,y: view.frame.maxY-200,width: view.frame.width,height: 200)
+        button = UIButton(frame: CGRect(x: view.frame.width-50, y: view.frame.maxY-200, width: 50, height: 40))
+        button.setTitleColor(donecolor, for: .normal)
+        button.setTitle("done", for: .normal)
+        button.addTarget(self, action: #selector(self.newButtonAction), for: .touchUpInside)
         view.addSubview(picker)
+        view.addSubview(button)
         picker.didMoveToSuperview()
-        super.init()
         picker.delegate = self
         picker.dataSource = self
-        
+        close()
     }
-    public init(_ view:UIView,_ item:[String],_ selectLinstener:@escaping (_ position:String) -> Void,_ color:UIColor,_ height:Int) {
+    @objc func newButtonAction(sender: UIButton!) {
+        close()
+        callback(item![picker.selectedRow(inComponent: 0)])
+    }
+    public init(_ view:UIView,_ item:[String],_ selectLinstener:@escaping (_ position:String) -> Void,_ color:UIColor,_ height:Int,_ donecolor:UIColor=UIColor.black) {
         super.init()
         callback=selectLinstener
         self.item=item
@@ -35,8 +45,14 @@ public class SimplePicker:NSObject,UIPickerViewDelegate,UIPickerViewDataSource{
         picker.dataSource=self
         picker.backgroundColor = color
         picker.frame=CGRect(x: 0,y: view.frame.maxY-CGFloat(height),width: view.frame.width,height: CGFloat(height))
+        button =  UIButton(frame: CGRect(x: view.frame.width-50, y: view.frame.maxY-CGFloat(height), width: 50, height: 40))
+        button.setTitle("done", for: .normal)
+        button.setTitleColor(donecolor, for: .normal)
+        button.addTarget(self, action: #selector(self.newButtonAction), for: .touchUpInside)
         view.addSubview(picker)
+        view.addSubview(button)
         picker.didMoveToSuperview()
+        close()
     }
     public func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -52,16 +68,21 @@ public class SimplePicker:NSObject,UIPickerViewDelegate,UIPickerViewDataSource{
     public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print("select\(pickerView.selectedRow(inComponent: 0))")
         callback(item[pickerView.selectedRow(inComponent: 0)])
-        visible(false)
+        close()
     }
     
     public func notifyDataSetChange(){
         picker.reloadAllComponents()
     }
-    public func visible(_ a:Bool){
-        picker.isHidden = !a
+    public func open(){
+        picker.isHidden = false
+        if(button != nil){button.isHidden = false}
     }
-    
+    public func close(){
+        picker.isHidden = true
+        if(button != nil){button.isHidden = true}
+        
+    }
     public func resetitem(_ item:[String]){
         self.item=item
         notifyDataSetChange()
